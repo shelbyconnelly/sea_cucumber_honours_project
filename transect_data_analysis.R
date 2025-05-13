d4101055efe7df589752c18ae89c8f44d4c14e3c
sea-shellby <shelby.connelly@icloud.com>
2025-05-13 13:52:23 -0700

    Updated ambient density and biomass plots.

--- transect_data_analysis.R
+++ transect_data_analysis.R
@@ -1,17 +1,13 @@
 # Project: Sea cucumber honours project
 # Analyzing transect data
 # Author: Shelby Connelly
-# Date: 03/20/2025 - 04/12/2025
+# Date: 03/20/2025 - 05/13/2025
 
 # Installing packages
-install.packages(c("tidyverse", "glmmTMB", "emmeans", "DHARMa", "patchwork"))
+install.packages("tidyverse")
 
 # Loading packages into R
 library(tidyverse)
-library(glmmTMB)
-library(emmeans)
-library(DHARMa)
-library(patchwork)
 
 # Loading transect data into R
 clean_transect_data <- read_csv("./clean_data/clean_transect_data.csv")
@@ -36,8 +32,14 @@ sea_cucumber_transect_data <- sea_cucumber_transect_data %>%
 
 # DENSITY DATA -----------------------------------------------------------------
 
-# Plotting sea cucumber density by site
-site_density_plot <- ggplot(sea_cucumber_transect_data,
+# Plotting ambient sea cucumber density by site and saving as a .tiff file
+tiff("./plots/ambient_density_plot.tiff",
+     height = 2.5,
+     width = 5,
+     units = "in",
+     res = 600)
+
+ambient_density_plot <- ggplot(sea_cucumber_transect_data,
                             aes(x = site, 
                                 y = density)) + 
   theme_classic() +
@@ -47,44 +49,38 @@ site_density_plot <- ggplot(sea_cucumber_transect_data,
   stat_summary(fun.data = mean_se, 
                geom = "errorbar", 
                width = 0.25) +
-  theme(axis.title.x = element_blank(),
-        axis.text.x = element_blank()) +
-  labs(y = bquote(Density~("#"/m^2)))
-       
-site_density_plot
+  labs(x = NULL,
+       y = expression(paste("Ambient density (#/", m^2, ")"))) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))
+ 
+ambient_density_plot
+
+dev.off()
 
 # BIOMASS DATA -----------------------------------------------------------------
 
-# Plotting sea cucumber biomass by site
-site_biomass_plot <- ggplot(sea_cucumber_transect_data,
-                       aes(x = site,
-                           y = total_biomass)) +
+# Plotting ambient sea cucumber biomass by site and saving as a .tiff file
+tiff("./plots_ambient_biomass_plot.tiff",
+     height = 2.5,
+     width = 5,
+     units = "in",
+     res = 600)
+
+ambient_biomass_plot <- ggplot(sea_cucumber_transect_data,
+                            aes(x = site,
+                                y = total_biomass)) +
   theme_classic() +
-  geom_jitter(width = 0.1, 
+  geom_jitter(width = 0.1,
               alpha = 0.25) +
   stat_summary(fun = mean) +
   stat_summary(fun.data = mean_se, 
                geom = "errorbar", 
                width = 0.25) +
-  annotate("text", x = c("boulder_island", "best_point"), y = 0, label = "N/A") +
-  labs(x = "Site",
-       y = "Biomass (g)") +
-  scale_x_discrete(labels = c("Boulder Island", "Jug Island", "Twin Islands", 
-                              "Brighton Beach", "Old Buntzen PP", "Best Point", 
-                              "S Johnson Bay", "S Croker Island", "N Croker Island")) +
+  annotate("text", x = "Best Point", y = 0, label = "N/A") +
+  labs(x = NULL,
+       y = expression(paste("Ambient biomass (g/", m^2, ")"))) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))
 
-site_biomass_plot
-
-# Merging site density and size plots and saving as a .tiff file ---------------
-tiff(file = "./plots/transect_density_biomass_plot.tiff", 
-     height = 5, 
-     width = 5.5, 
-     units = "in", 
-     res = 400)
-
-transect_density_biomass_plot <- site_density_plot / site_biomass_plot
-
-transect_density_biomass_plot
+ambient_biomass_plot
 
 dev.off()
